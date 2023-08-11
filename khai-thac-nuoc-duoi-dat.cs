5dc7278e215d8f5b0b99eeaf9c68e214790c3a27
tdquang266 <72800859+tdquang266@users.noreply.github.com>
2023-08-10 23:47:40 -0700

    Add files via upload

---
+++ khai-thac-nuoc-duoi-dat.cs
@@ -0,0 +1,15 @@
+﻿namespace khai_thac_nuoc_duoi_dat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
