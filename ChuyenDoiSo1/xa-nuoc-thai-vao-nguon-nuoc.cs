cde60f7fe84dba22b8df59369ce5bd9cae3ed7d7
tdquang266 <72800859+tdquang266@users.noreply.github.com>
2023-08-10 23:40:56 -0700

    Add files via upload

---
+++ ChuyenDoiSo1/xa-nuoc-thai-vao-nguon-nuoc.cs
@@ -0,0 +1,18 @@
+﻿namespace xa_nuoc_thai_vao_nguon_nuoc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Panel bottomMarginPanel = new Panel();
+            bottomMarginPanel.Dock = DockStyle.Bottom;
+            bottomMarginPanel.Height = 20; // Chiều cao của khoảng trống
+            this.Controls.Add(bottomMarginPanel);
+        }
+    }
+}
